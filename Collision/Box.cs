c31f8615c44cd57ab0316d6d0e4cbdb500578efa
ouo2u <abod98451@gmail.com>
2024-01-18 10:50:15 +0100

    yaaa

--- Collision/Box.cs
+++ Collision/Box.cs
@@ -7,12 +7,17 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace Collision
 {
-    public class Box
+    public abstract class Box
     {
         protected Texture2D texture;
         protected Vector2 position;
         protected Rectangle hitbox;
         protected Color color;
+
+        public Rectangle Hitbox{
+            get{return hitbox;}
+            set{Hitbox=value;}
+        }
 
         public Box(Texture2D texture, Vector2 startPosition)
         {
@@ -22,10 +27,9 @@ namespace Collision
             color = Color.White;
         }
 
-        public virtual void Update(){
-        }
+        public abstract void Update();
         
-        public void Draw(SpriteBatch spriteBatch){
+        public virtual void Draw(SpriteBatch spriteBatch){
             spriteBatch.Draw(texture,hitbox,color);
         }
     }
